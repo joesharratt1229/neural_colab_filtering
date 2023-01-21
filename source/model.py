79aea213d8f62436d3d1df3d3fb39f5d16d4ea8a
joesharratt1229 <joesharratt1229@gmail.com>
2023-01-21 14:11:33 +0000

    Began creating initial model

--- source/model.py
+++ source/model.py
@@ -11,6 +11,7 @@ class NCF(nn.Module):
         self.embedding_user = nn.Embedding(num_users, embed_dim)
         self.embedding_item = nn.Embedding(num_films, embed_dim)
         self.MLP = self._MLP(hidden_layers, embed_dim, dropout_rate)
+        self._init_params()
 
     def _MLP(self, hidden_layer_unit, embed_unit, dropout):
         assert hidden_layer_unit == 2 * embed_unit
@@ -25,10 +26,20 @@ class NCF(nn.Module):
         
         hidden_layers.append(nn.Linear(hidden_layer_unit[-1], 1))
         hidden_layers.append(nn.Sigmoid())
-        
+
         return nn.Sequential(*hidden_layers)
 
-
+    def _init_params(self):
+        def _weights_init(m):
+            if type(m) == nn.Linear:
+                nn.init.xavier_uniform_(m.weight)
+                m.bias.data.fill_(0.0)
+            self.embedding_user.weight.uniform_(-.05, .05)
+            self.embedding_item.weight.uniform_(-.05, .05)
+            self.MLP.apply(_weights_init)
+    
+    
+            
     def forward(self, user_indices, item_indices):
         user_embedding = self.embedding_user(user_indices)
         item_embedding = self.embedding_item(item_indices)
