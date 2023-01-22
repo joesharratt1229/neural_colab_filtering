2df068a0dff280a722068121f444a7d2e5d5acdf
joesharratt1229 <joesharratt1229@gmail.com>
2023-01-22 14:50:24 +0000

    changed dataset

--- source/model.py
+++ source/model.py
@@ -11,7 +11,13 @@ class NCF(nn.Module):
         self.embedding_user = nn.Embedding(num_users, embed_dim)
         self.embedding_item = nn.Embedding(num_films, embed_dim)
         self.MLP = self._MLP(hidden_layers, embed_dim, dropout_rate)
+        if dropout_rate:
+            self.dropout = nn.Dropout(dropout_rate)
         self._init_params()
+
+        assert output_range and len(output_range) == 2
+        self.norm_min = min(output_range)
+        self.norm_range = abs(output_range[1] - output_range[0]) +1
 
     def _MLP(self, hidden_layer_unit, embed_unit, dropout):
         assert hidden_layer_unit == 2 * embed_unit
@@ -26,7 +32,6 @@ class NCF(nn.Module):
         
         hidden_layers.append(nn.Linear(hidden_layer_unit[-1], 1))
         hidden_layers.append(nn.Sigmoid())
-
         return nn.Sequential(*hidden_layers)
 
     def _init_params(self):
@@ -39,10 +44,20 @@ class NCF(nn.Module):
             self.MLP.apply(_weights_init)
     
     
-            
     def forward(self, user_indices, item_indices):
         user_embedding = self.embedding_user(user_indices)
         item_embedding = self.embedding_item(item_indices)
-        torch.cat([user_embedding, item_embedding], dim=-1)
+        x = torch.cat([user_embedding, item_embedding], dim=-1)
+
+        if hasattr(self, 'dropout'):
+            x = self.dropout(x)
+        x = self.MLP(x)
+        normalised_output = x * self.norm_range + self.norm_min
+        return normalised_output
+
+
+
+
+
 
 
